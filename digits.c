29462ab01f967fb8685dcdd588c7407f8e2528c0
a1940206 <1043684678@qq.com>
2025-10-07 17:23:04 +0800

    Create digits.c

---
+++ digits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int x;
+  if (scanf("%d", &x) != 1) return 1;   // assume positive three-digit integer
+
+  int ones = x % 10;
+  int tens = (x / 10) % 10;
+  int hundreds = (x / 100) % 10;
+
+  printf("%d\n%d\n%d\n", ones, tens, hundreds);
+  return 0;
+}
