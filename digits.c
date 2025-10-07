30d05e664e9cacff9b37c07f25810cfaae2805de
a1940206 <1043684678@qq.com>
2025-10-07 17:29:44 +0800

    Update digits.c

--- digits.c
+++ digits.c
@@ -2,7 +2,7 @@
 
 int main(void) {
   int x;
-  if (scanf("%d", &x) != 1) return 1;   // assume positive three-digit integer
+  if (scanf("%d", &x) != 1) return 1;  
 
   int ones = x % 10;
   int tens = (x / 10) % 10;
