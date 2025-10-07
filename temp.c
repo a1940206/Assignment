0921b8c1d82561a32a17ae43a4334e0ac0901f1e
a1940206 <1043684678@qq.com>
2025-10-07 17:27:51 +0800

    Update temp.c

--- temp.c
+++ temp.c
@@ -5,7 +5,8 @@ int main(void) {
   printf("Enter temperature in Celsius: ");
   if (scanf("%d", &c) != 1) return 1;
 
-  double f = c * 9.0 / 5.0 + 32.0;   // use floating-point to avoid 9/5 truncation
+  double f =
+      c * 9.0 / 5.0 + 32.0; 
   printf("%d degrees Celsius is %.0f degrees Fahrenheit\n", c, f);
   return 0;
 }
