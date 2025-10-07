2b62b475cd32f29a8f4f4a134ed36ef0d968e08d
a1940206 <1043684678@qq.com>
2025-10-07 17:21:07 +0800

    Create temp.c

---
+++ temp.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int c;
+  printf("Enter temperature in Celsius: ");
+  if (scanf("%d", &c) != 1) return 1;
+
+  double f = c * 9.0 / 5.0 + 32.0;   // use floating-point to avoid 9/5 truncation
+  printf("%d degrees Celsius is %.0f degrees Fahrenheit\n", c, f);
+  return 0;
+}
