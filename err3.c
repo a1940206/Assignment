fd321717f8b30c2a2f57f16fac2194898365f07f
a1940206 <1043684678@qq.com>
2025-10-07 17:31:46 +0800

    Update err3.c

--- err3.c
+++ err3.c
@@ -3,7 +3,7 @@
 int main(void) {
   int x = 0;
   printf("enter a number and I'll double it: ");
-  scanf("%d", &x);
+  if (scanf("%d", &x) != 1) return 1;
   printf("twice %d is %d\n", x, 2 * x);
   return 0;
 }
