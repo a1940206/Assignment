19c26abf147ad5c4f14ceb540a7454da339c4f64
a1940206 <1043684678@qq.com>
2025-10-07 17:28:54 +0800

    Update form_letter.c

--- form_letter.c
+++ form_letter.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-  char first[11], last[11];   // up to 10 letters + null terminator
+  char first[11], last[11];  
   int age;
   double frac;
 
   printf("Enter your name, age and employment fraction: ");
   if (scanf("%10s %10s %d %lf", first, last, &age, &frac) != 4) return 1;
 
-  printf("Your name is %s %s, you are %d years old and your employment fraction is %.1f.\n",
-         first, last, age, frac);
+  printf(
+      "Your name is %s %s, you are %d years old and your employment fraction "
+      "is %.1f.\n",
+      first, last, age, frac);
   return 0;
 }
